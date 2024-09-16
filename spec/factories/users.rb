c87ebae3599106b473f5978c7d06c5565a58088e
u-nami <chikayym@gmail.com>
2024-09-16 15:29:10 +0900

    FactoryBotの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname                 {Faker::Name}
+    email                    {Faker::Internet.email}
+    password                 {Faker::Internet.password(min_length:6)}
+    password_confirmation    {password}
+    kanji_family             {Faker::Name.last_name}
+    kanji_name               {Faker::Name.first_name}
+    kata_family              {"タナカ"}
+    kata_name                {"タロウ"}
+    birthday                 {Faker::Date.birthday}
   end
 end
