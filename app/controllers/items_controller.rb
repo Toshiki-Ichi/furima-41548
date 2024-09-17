112f79c0b66c47366d2e88e1607f8a5fd2bbba8e
u-nami <chikayym@gmail.com>
2024-09-17 15:43:54 +0900

    商品登録画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:new]
   def index
   end
-end
+
+  def new
+    
+  end
+  private
+	def move_to_index
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
+  end
+  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
  has_one_attached :image
 belongs_to :user
+
+validates :image, :price, :category_id, :status_id, :responsible_id, region_id, shipping_day_id: true
 end
