bf21fa7de4b57d56ba85085fd3fb36f2af0258b4
u-nami <chikayym@gmail.com>
2024-09-17 16:45:20 +0900

    AvtiveHash項目の記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
- has_one_attached :image
+ActiveHash::Associations::ActiveRecordExtensions 
+	has_one_attached :image
 belongs_to :user
+belongs_to :genre
 
-validates :image, :price, :category_id, :status_id, :responsible_id, region_id, shipping_day_id: true
+
+validates :image, presence: true
+validates :price, :category_id, :status_id, :responsible_id, region_id, shipping_day_id, presence: true
+validates :genre_id, numericality: { other_than: 1 }
 end
