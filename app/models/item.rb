3c766ffae5430429e91f39b2faaed00384dc8e87
u-nami <chikayym@gmail.com>
2024-09-18 23:14:51 +0900

    単体テストの実装およびバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,7 @@ include ActiveHash::Associations::ActiveRecordExtensions
 	has_one_attached :image
 belongs_to :user
 
-
-validates :image, presence: true
-validates :price, :category_id, :status_id, :responsible_id, :region_id, :shipping_day_id, presence: true
-validates :category_id, numericality: { other_than: 1 }
-validates :region_id, numericality: { other_than: 1 }
-validates :responsible_id, numericality: { other_than: 1 }
-validates :shipping_day_id, numericality: { other_than: 1 }
-validates :status_id, numericality: { other_than: 1 }
+validates :image, :item_name, :note, :price, presence: true
+  validates :category_id, :status_id, :responsible_id, :region_id, :shipping_day_id, presence: true, numericality: { other_than: 1 }
+	validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
