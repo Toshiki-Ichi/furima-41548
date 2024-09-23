73dd6b9d544078b2a9addc57dfda9ca1b2c356e6
u-nami <chikayym@gmail.com>
2024-09-23 18:06:21 +0900

    コントローラー名の変更orders→customers

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,5 @@
+class CustomersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id]) 
+  end
+end
--- app/controllers/orders_controller.rb
+++
@@ -1,6 +0,0 @@
-class OrdersController < ApplicationController
-	def index
-		@order = Item.find(params[:item_id]) 
-    
-	end
-end
