1090e5c1ff2b81a6d77f0a5690cec674f39df6e3
u-nami <chikayym@gmail.com>
2024-10-30 14:24:23 +0900

    要素のclass名と適応後のレイアウト修正

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbo:load', function(){
   // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('.click-upload');
+	const postForm = document.querySelector('.click-upload');
   // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
@@ -37,13 +37,14 @@ document.addEventListener('turbo:load', function(){
 
   // file_fieldを生成・表示する関数
   const buildNewFileField = () => {
+
     // 2枚目用のfile_fieldを作成
     const newFileField = document.createElement('input');
     newFileField.setAttribute('type', 'file');
-    newFileField.setAttribute('id', 'item-image');
-
+    newFileField.setAttribute('name', 'item[images][]');
     // 最後のfile_fieldを取得
-    const lastFileField = document.querySelector('input[type="file"][name="post[images][]"]:last-child');
+		const lastFileField = document.querySelector('input[type="file"][name="item[images][]"]:last-child');
+
     // nextDataIndex = 最後のfile_fieldのdata-index + 1
     const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
     newFileField.setAttribute('data-index', nextDataIndex);
@@ -73,8 +74,13 @@ document.addEventListener('turbo:load', function(){
     // data-index（何番目を操作しているか）を取得
     const dataIndex = e.target.getAttribute('data-index');
 
+		const file = e.target.files[0];
 
-    const file = e.target.files[0];
+		 // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
+		 if (!file) {
+      deleteImage(dataIndex);
+      return null;
+    };
     const blob = window.URL.createObjectURL(file);
 
  // data-indexを使用して、既にプレビューが表示されているかを確認する
@@ -94,7 +100,7 @@ document.addEventListener('turbo:load', function(){
   };
 
   // input要素を取得
-  const fileField = document.querySelector('input[type="file"][id="item-image"]');
+	const fileField = document.querySelector('input[type="file"][name="item[images][]"]');
 
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', changedFileField);
